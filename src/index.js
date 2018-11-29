6148c417ea4b41e6f4ed791ffc3ff82607be5d54
Alex Rankin <alex.c.rankin@gmail.com>
2018-11-29 10:50:49 -0800

    making some progress i think

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 export const isPromise = payload => {
-  return typeof payload.then === 'function'
+  return payload && typeof payload.then === 'function'
 };
+
+export const middleware = store => next => action => {
+  // Check if the payload is a promise using the isPromise function
+  if(!isPromise(action.payload)) return next(action);
+  //dispatch new loading action
+  store.dispatch({
+    type: 'LOAD_START'
+  });
+
+  return action.payload
+    .then(result => {
+    //do the promise action
+    })
+    .catch(err => {
+      //dispatch DONE_LOADING
+      store.dispatch({ type: 'LOAD_END' })
+      //dispatch the error action
+      store.dispatch({ type: 'ERROR', payload: err });
+    })
+
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
-import { isPromise } from './index';
+import { isPromise, middleware } from './index';
+import { createStore, applyMiddleware } from 'redux';
 
 describe('isPromise', () => {
   test('it returns true if passed a promise', () => {
     const promise = new Promise((resolve, reject) => {
       console.log('foo');
     });
-
     expect(isPromise(promise)).toEqual(true);
   });
 
@@ -14,3 +14,20 @@ describe('isPromise', () => {
     expect(isPromise(notPromise)).toEqual(false);
   })
 });
+
+describe('promise middleware', () => {
+  const reducer = jest.fn((state) => state);
+  const store = createStore(reducer, {}, applyMiddleware(middleware));
+
+  test('resolves promise and pushes payload to the reducer', () => {
+    const promise = Promise.resolve('ouch');
+    store.dispatch({
+      type: 'PROMISE_ACTION',
+      payload: promise
+    });
+
+    return promise.then(() => {
+      expect(reducer.mock.calls).toContain([{}, { type: 'PROMISE_ACTION', payload: 'ouch'}])
+    })
+  })
+})
