b1cff9255a46a8dfe2908e1104a22775569c7bf4
Alex Rankin <alex.c.rankin@gmail.com>
2018-11-29 11:58:51 -0800

    all tests passing

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ export const isPromise = payload => {
 };
 
 export const middleware = store => next => action => {
-  // Check if the payload is a promise using the isPromise function
   if(!isPromise(action.payload)) return next(action);
-  //dispatch new loading action
   store.dispatch({
     type: 'LOAD_START'
   });
 
   return action.payload
     .then(result => {
-    //do the promise action
+      store.dispatch({ type: 'LOAD_END' });
+      store.dispatch({ type: 'PROMISE_ACTION', payload: result });
     })
     .catch(err => {
-      //dispatch DONE_LOADING
-      store.dispatch({ type: 'LOAD_END' })
-      //dispatch the error action
+      store.dispatch({ type: 'LOAD_END' });
       store.dispatch({ type: 'ERROR', payload: err });
     })
-
-}
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,25 +1,27 @@
 import { isPromise, middleware } from './index';
 import { createStore, applyMiddleware } from 'redux';
+import { BADFAMILY } from 'dns';
 
 describe('isPromise', () => {
-  test('it returns true if passed a promise', () => {
+  it('it returns true if passed a promise', () => {
     const promise = new Promise((resolve, reject) => {
       console.log('foo');
     });
     expect(isPromise(promise)).toEqual(true);
   });
 
-  test('it returns false if passed anything other than a promise', () => {
+  it('it returns false if passed anything other than a promise', () => {
     const notPromise = { hair: 'red', eyes: 'blue' };
     expect(isPromise(notPromise)).toEqual(false);
-  })
+  });
 });
 
 describe('promise middleware', () => {
-  const reducer = jest.fn((state) => state);
-  const store = createStore(reducer, {}, applyMiddleware(middleware));
 
-  test('resolves promise and pushes payload to the reducer', () => {
+  it('passes a LOAD_START action to the reducer', () => {
+    const reducer = jest.fn((state) => state);
+    const store = createStore(reducer, {}, applyMiddleware(middleware));
+
     const promise = Promise.resolve('ouch');
     store.dispatch({
       type: 'PROMISE_ACTION',
@@ -27,7 +29,68 @@ describe('promise middleware', () => {
     });
 
     return promise.then(() => {
-      expect(reducer.mock.calls).toContain([{}, { type: 'PROMISE_ACTION', payload: 'ouch'}])
+      expect(reducer.mock.calls[1]).toEqual([{}, { type: 'LOAD_START' }])
+    });
+  });
+
+  it('passes the LOAD_END action for a successful promise', () => {
+    const reducer = jest.fn((state) => state);
+    const store = createStore(reducer, {}, applyMiddleware(middleware));
+
+    const promise = Promise.resolve('ouch');
+    store.dispatch({
+      type: 'PROMISE_ACTION',
+      payload: promise
+    });
+
+    return promise.then(() => {
+      expect(reducer.mock.calls[2]).toEqual([{}, { type: 'LOAD_END' }])
+    });
+
+  })
+
+  it('passes the PROMISE_ACTION for a successful promise', () => {
+    const reducer = jest.fn((state) => state);
+    const store = createStore(reducer, {}, applyMiddleware(middleware));
+
+    const promise = Promise.resolve('ouch');
+    store.dispatch({
+      type: 'PROMISE_ACTION',
+      payload: promise
+    });
+
+    return promise.then(() => {
+      expect(reducer.mock.calls[3]).toEqual([{}, { type: 'PROMISE_ACTION', payload: 'ouch' }])
+    });
+  });
+
+  it('passes the LOAD_END action for an unsuccessful promise', () => {
+    const reducer = jest.fn((state) => state);
+    const store = createStore(reducer, {}, applyMiddleware(middleware));
+    const FAKE_ACTION = 'FAKE_ACTION'
+
+    const badPromise = Promise.reject(new Error('fail'));
+    return middleware(store)(() => {})({
+      type: FAKE_ACTION,
+      payload: badPromise
+    })
+      .finally(() => {
+        expect(reducer.mock.calls[2]).toEqual([{}, { type: 'LOAD_END' }])
+    });
+  });
+
+  it('passes the ERROR action', () => {
+    const reducer = jest.fn((state) => state);
+    const store = createStore(reducer, {}, applyMiddleware(middleware));
+    const FAKE_ACTION = 'FAKE_ACTION'
+
+    const badPromise = Promise.reject(new Error('fail'));
+    return middleware(store)(() => {})({
+      type: FAKE_ACTION,
+      payload: badPromise
+    })
+      .finally(() => {
+      expect(reducer.mock.calls[3]).toEqual([{}, { type: 'ERROR', payload: new Error('fail')}])
     })
   })
-})
+});
