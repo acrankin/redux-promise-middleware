ece5d17127a2a47e86c2e92e20ac787ee09bc908
Alex Rankin <alex.c.rankin@gmail.com>
2018-11-28 17:34:59 -0800

    isPromise tested

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,16 @@
 import { isPromise } from './index';
 
 describe('isPromise', () => {
-  
-})
+  test('it returns true if passed a promise', () => {
+    const promise = new Promise((resolve, reject) => {
+      console.log('foo');
+    });
+
+    expect(isPromise(promise)).toEqual(true);
+  });
+
+  test('it returns false if passed anything other than a promise', () => {
+    const notPromise = { hair: 'red', eyes: 'blue' };
+    expect(isPromise(notPromise)).toEqual(false);
+  })
+});
